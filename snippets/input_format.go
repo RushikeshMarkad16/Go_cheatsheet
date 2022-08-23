5142a8e47ff4224a49a26892a285ebe1c922fb5f
Rushikesh Markad <60238321+RushikeshMarkad16@users.noreply.github.com>
2022-08-23 12:42:23 +0530

    Create input_format.go

---
+++ snippets/input_format.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	//"bufio"
+	"fmt"
+	//"os"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	var str string
+	fmt.Scanln(&str)
+	fmt.Println(str)
+// 	reader := bufio.NewReader(os.Stdin)
+// 	input, err := reader.ReadString('\n')
+// 	fmt.Println(input)
+// 	fmt.Println(err)
+}
