bb4d041ece074a74d57cc5f80b09f3fa2083dae3
Rushikesh Markad <60238321+RushikeshMarkad16@users.noreply.github.com>
2023-07-14 13:13:24 +0530

    Added comment

--- snippets/input_format.go
+++ snippets/input_format.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	var str string
-	fmt.Scanln(&str)
+	fmt.Scanln(&str)   //for single word
 	fmt.Println(str)
-// 	reader := bufio.NewReader(os.Stdin)
+// 	reader := bufio.NewReader(os.Stdin)    //for sentence
 // 	input, err := reader.ReadString('\n')
 // 	fmt.Println(input)
 // 	fmt.Println(err)
